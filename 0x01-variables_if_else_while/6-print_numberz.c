262acd6b88ceac2f32c7c1fcb9866969ef346e06
lymisoft <lymisoft@gmail.com>
2023-06-16 07:49:08 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = '0'; l <= '9'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
