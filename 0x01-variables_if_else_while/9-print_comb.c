4ef6793295861db9666fc85831de9ebd92c5fb3d
lymisoft <lymisoft@gmail.com>
2023-06-16 08:42:04 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ls;
+
+	for (ls = 0; ls < 10; ls++)
+		putchar(ls + '0');
+		if (a < 9)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+	return (0);
+}
