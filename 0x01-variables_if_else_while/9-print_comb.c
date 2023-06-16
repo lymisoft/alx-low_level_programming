7fcef999ae1180274a5e068f4f946aea562893cd
lymisoft <lymisoft@gmail.com>
2023-06-16 08:51:08 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,14 @@ int main(void)
 	int ls;
 
 	for (ls = 0; ls < 10; ls++)
+	{
 		putchar(ls + '0');
 		if (a < 9)
+		{
 			putchar(',');
 			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
