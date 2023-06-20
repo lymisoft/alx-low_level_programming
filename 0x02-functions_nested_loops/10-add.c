1da85174cd80766f0bdbd4a6738343fde0963245
lymisoft <lymisoft@gmail.com>
2023-06-20 08:45:29 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result
+ * @l: the first integer to add
+ * @s: the second integer to add
+ * Return: sum of l and s
+ */
+
+int add(int l, int s)
+{
+	int t;
+
+	t = l + s;
+
+	return (t);
+}
