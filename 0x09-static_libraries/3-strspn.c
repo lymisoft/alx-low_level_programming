73d8ffceea633906547dd187c28a9fa8b56131ae
lymisoft <lymisoft@gmail.com>
2023-07-07 19:14:20 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int l;
+
+	while (*s)
+	{
+		for (l = 0; accept[l]; l++)
+		{
+			if (*s == accept[l])
+			{
+				n++;
+				break;
+			}
+			else if (accept[l + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
